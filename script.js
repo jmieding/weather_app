0c8d43a913cce4df45d78fe5924d760df0a7ec02
JMieding <jtmieding@gmail.com>
2016-04-20 12:38:29 -0500

    try again

--- script.js
+++ script.js
@@ -57,8 +57,7 @@ window.onload = function() {
   } else {
     document.body.style.backgroundImage = "url('winter-20234.jpg')";
     document.getElementById('temp').style.color = "black";
-    var h2 = document.getElementsByTagName('h2');
-    h2.style.color = "black";
+    document.getElementById('header').style.color = "black";
   };
   // Download Forecast Data
   //var requestForecast = new XMLHttpRequest();

756a5b6704fa0c4bfcf45ab11c26a1029a205164
JMieding <jtmieding@gmail.com>
2016-04-19 20:54:57 -0500

    background image testing vertical no-repeat

--- script.js
+++ script.js
@@ -47,14 +47,14 @@ window.onload = function() {
 
   var body = document.getElementsByTagName('body')[0];
   if (temp >= 88) {
-    body.style.backgroundImage = 'url(desert-1007157.jpg)';
+    body.style.backgroundImage = 'url(desert-1007157.jpg) no-repeat center center fixed';
   } else if (temp >= 67 && temp < 88) {
-    body.style.backgroundImage = 'url(beach-656734.jpg)';
+    body.style.backgroundImage = 'url(beach-656734.jpg) no-repeat center center fixed';
   } else if (temp >= 50 && temp < 67) {
-    body.style.backgroundImage = 'url(lake-65443.jpg)';
+    body.style.backgroundImage = 'url(lake-65443.jpg) no-repeat center center fixed';
   } else if (temp > 38 && temp < 50) {
-    body.style.backgroundImage = 'url(forest-605505.jpg)';
+    body.style.backgroundImage = 'url(forest-605505.jpg) no-repeat center center fixed';
   } else {
-    body.style.backgroundImage = 'url(winter-20234.jpg)';
+    body.style.backgroundImage = 'url(winter-20234.jpg) no-repeat center center fixed';
   }
 };
